c3e39f2993f60b27f32683511a8443c1454a9b50
Anshul Gupta <hi.anshulg@gmail.com>
2024-02-01 23:49:58 -0500

    increase loading time

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const src = img.getAttribute("data-src");
+        img.setAttribute("src", src);
+        observer.unobserve(img);
+      }
+    });
+  });
+
+  document.querySelectorAll("img[data-src]").forEach((img) => {
+    observer.observe(img);
+  });
+});
+
 window.addEventListener("DOMContentLoaded", () => {
   const ctl = new CollapsibleTimeline("#timeline");
 });
